66c5b490eccb669d20108a649cf4fed21553a28a
Brad Smith <bradster@infinitewarp.com>
2017-09-06 23:46:13 -0400

    move speaker limiting to method call from init

--- trekipsum/backends/sqlite.py
+++ trekipsum/backends/sqlite.py
@@ -3,7 +3,7 @@ import random
 import sqlite3
 from os import path
 
-from ..exceptions import NoDialogFoundException, SpeakerNotFoundException
+from ..exceptions import NoDialogFoundException
 
 logger = logging.getLogger(__name__)
 
@@ -19,11 +19,13 @@ class DialogChooser(object):
     SQL_GET_RANDOM = 'SELECT speaker, line FROM dialog ORDER BY dialog_id LIMIT 1 OFFSET ?'
     SQL_GET_RANDOM_BY_SPEAKER = 'SELECT speaker, line FROM dialog WHERE speaker = ?' \
                                 'ORDER BY dialog_id LIMIT 1 OFFSET ?'
+    SQL_GET_RANDOM_SPEAKER = 'SELECT DISTINCT speaker FROM dialog ORDER BY RANDOM() LIMIT 1'
+    SQL_GET_RANDOM_SPEAKER_EXCLUDING = 'SELECT DISTINCT speaker FROM dialog WHERE speaker <> ?' \
+                                       'ORDER BY RANDOM() LIMIT 1'
 
-    def __init__(self, speaker=None):
+    def __init__(self):
         """Initialize with no dialog and default sqlite path."""
-        self.speaker = speaker.upper() if speaker else None
-        self._dialog_count = 0
+        self._dialog_counts = {}
         self._sqlite_path = DEFAULT_SQLITE_PATH
         self._conn = sqlite3.connect(self._sqlite_path)
 
@@ -36,36 +38,45 @@ class DialogChooser(object):
     def __del__(self, *args):
         self._conn.close()
 
-    @property
-    def dialog_count(self):
+    def dialog_count(self, speaker=None):
         """Lazy-load and return count of lines."""
-        if self._dialog_count == 0:
-            if self.speaker is None:
-                self._dialog_count = self._conn.execute(self.SQL_COUNT).fetchone()[0]
+        speaker = speaker.upper() if speaker else None
+        if speaker not in self._dialog_counts:
+            if speaker is not None:
+                count = self._conn.execute(self.SQL_COUNT_BY_SPEAKER,
+                                           (speaker,)).fetchone()[0]
             else:
-                self._dialog_count = self._conn.execute(self.SQL_COUNT_BY_SPEAKER,
-                                                        (self.speaker,)).fetchone()[0]
-        return self._dialog_count
+                count = self._conn.execute(self.SQL_COUNT).fetchone()[0]
+            self._dialog_counts[speaker] = count
+        return self._dialog_counts[speaker]
 
-    def random_dialog(self):
+    def random_dialog(self, speaker=None):
         """
-        Get random line of dialog.
+        Get random line of dialog, optionally limited to specific speaker.
 
         Returns:
             tuple containing (speaker name, line of dialog)
         """
-        if self.dialog_count == 0:
-            if self.speaker is not None:
-                raise SpeakerNotFoundException(self.speaker)
-            else:
-                raise NoDialogFoundException()
+        speaker = speaker.upper() if speaker else None
+        if self.dialog_count(speaker) == 0:
+            raise NoDialogFoundException(speaker)
 
         logger.debug('choosing random from count %s', self.dialog_count)
-        offset = random.randrange(self.dialog_count)
-        if self.speaker is None:
+        offset = random.randrange(self.dialog_count(speaker))
+        if speaker is None:
             speaker, line = self._conn.execute(self.SQL_GET_RANDOM,
                                                (offset,)).fetchone()
         else:
             speaker, line = self._conn.execute(self.SQL_GET_RANDOM_BY_SPEAKER,
-                                               (self.speaker, offset)).fetchone()
+                                               (speaker, offset)).fetchone()
         return speaker, line
+
+    def random_speaker(self, not_speaker=None):
+        """
+        Get random speaker name, optionally excluding specific speaker from candidacy.
+        """
+        if not_speaker is None:
+            return self._conn.execute(self.SQL_GET_RANDOM_SPEAKER).fetchone()[0]
+        else:
+            return self._conn.execute(self.SQL_GET_RANDOM_SPEAKER_EXCLUDING,
+                                      (not_speaker.upper(),)).fetchone()[0]
